8cee873f8c21ba13e08fe3beeff06619cdc9940a
ashfiaKhatun <ashfia1204@gmail.com>
2024-04-30 02:19:17 +0600

    updated coda

--- src/pages/Home/MyListCard.jsx
+++ src/pages/Home/MyListCard.jsx
@@ -17,15 +17,16 @@ const MyListCard = (props) => {
             confirmButtonText: "Yes, delete it!"
         }).then((result) => {
             if (result.isConfirmed) {
-                fetch(`http://localhost:5000/allItems/${_id}`, {
+                fetch(`http://localhost:5000/allItems/${data._id}`, {
                     method: "DELETE"
                 })
                     .then(res => res.json())
                     .then(data => {
+                        console.log(data);
                         if (data.deletedCount > 0) {
                             Swal.fire({
                                 title: "Deleted!",
-                                text: "Spot has been deleted.",
+                                text: "Item has been deleted.",
                                 icon: "success",
                             });
                             const reamining = craftItems.filter(craftItem => craftItem._id !== data._id);
@@ -36,36 +37,38 @@ const MyListCard = (props) => {
         });
     }
 
-
-
     return (
-        <div className="card card-compact shadow-xl border">
-            <figure className="p-4"><img src={data.photo} className="rounded-lg h-44 w-full" /></figure>
+        <div>
+            <div className="card card-compact shadow-xl border">
+                <figure className="p-4"><img src={data.photo} className="rounded-lg h-44 w-full" /></figure>
 
-            <div className="card-body">
-                <h2 className="card-title">{data.item_name}</h2>
+                <div className="card-body">
+                    <h2 className="card-title">{data.item_name}</h2>
 
-                <hr />
-                <div className="flex items-center text-lg">
-                    <p className="text-gray-700 ">{data.sub_category}</p>
-                    <p className="text-right"><b>Price: {data.price}</b></p>
+                    <hr />
+                    <div className="flex items-center text-lg">
+                        <p className="text-gray-700 ">{data.sub_category}</p>
+                        <p className="text-right"><b>Price: {data.price}</b></p>
 
-                </div>
-                <hr />
-                <p className="text-gray-700 ">Rating: {data.rating}</p>
-                <p className="text-gray-700 ">Time: {data.time} days</p>
+                    </div>
+                    <hr />
+                    <p className="text-gray-700 ">Rating: {data.rating}</p>
+                    <p className="text-gray-700 ">Time: {data.time} days</p>
 
-                <div className="card-actions">
-                    <Link to={`/update-item/${data._id}`}><button className="btn bg-gray-300 border-cyan-800 hover:bg-cyan-800 hover:text-white font-bold">Update</button></Link>
-                    <button onClick={() => handleItemDelete(data._id)} className="btn bg-gray-300 border-red-800 hover:bg-red-800 hover:text-white font-bold">Delete</button>
+                    <div className="card-actions">
+                        <Link to={`/update-item/${data._id}`}><button className="btn bg-gray-300 border-cyan-800 hover:bg-cyan-800 hover:text-white font-bold">Update</button></Link>
+                        <button onClick={() => handleItemDelete(data._id)} className="btn bg-gray-300 border-red-800 hover:bg-red-800 hover:text-white font-bold">Delete</button>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
 
 MyListCard.propTypes = {
-    props: PropTypes.object,
+    data: PropTypes.object,
+
 }
 
 export default MyListCard;
